f610c40aff8444ddfceb038db717e58989095fdb
Alek Kruk <kruk.aleksander@hotmail.com>
2021-02-10 12:19:36 +0100

    connect with firebase ok

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase';
 import 'firebase/auth';
 
+
+
 const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
