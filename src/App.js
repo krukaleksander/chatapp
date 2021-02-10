41980a2bcb223c3f9af4bd8ae69aa18d8d0ad2c6
Alek Kruk <kruk.aleksander@hotmail.com>
2021-02-10 12:00:13 +0100

    clone login authentication from different project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 
+import Signup from './components/Signup';
+import { Container } from 'react-bootstrap';
+import AppProvider from './context/AppContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import ForgotPassword from './components/ForgotPassword';
+import UpdateProfile from './components/UpdateProfile';
+
 const App = () => {
   return (
-    <>
-      <div className="chat">
-        <h1>Chat project</h1>
+
+    <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
+      <div className="w-100" style={{ maxWidth: '400px' }}>
+        <Router>
+          <AppProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
+              <Route path='/signup' component={Signup} />
+              <Route path='/login' component={Login} />
+              <Route path='/forgot-password' component={ForgotPassword} />
+            </Switch>
+          </AppProvider>
+        </Router>
       </div>
-    </>
-  );
+    </Container>
+    // 43:39
+
+
+  )
+
+
 }
 
 export default App;
---
+++ src/context/AppContext.js
@@ -0,0 +1,57 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { auth } from '../firebase';
+
+
+const AppContext = React.createContext();
+
+export function useAuth() {
+    return useContext(AppContext);
+};
+
+export default function AppProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState();
+    const [loading, setLoading] = useState(true);
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password);
+    };
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password);
+    };
+    function logout() {
+        return auth.signOut()
+    };
+    function resetPassword(email) {
+        return auth.sendPasswordResetEmail(email);
+    };
+    function updateEmail(email) {
+        return currentUser.updateEmail(email);
+    };
+    function updatePassword(password) {
+        return currentUser.updatePassword(password);
+    };
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user);
+            setLoading(false)
+            return unsubscribe
+        });
+    }, [])
+
+
+    const value = {
+        currentUser,
+        signup,
+        login,
+        logout,
+        resetPassword,
+        updateEmail,
+        updatePassword
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {!loading && children}
+        </AppContext.Provider>
+    )
+}
