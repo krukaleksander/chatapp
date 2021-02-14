01e53b311a9030d1369d073770c3eb3b588cae65
Alek Kruk <kruk.aleksander@hotmail.com>
2021-02-14 17:42:17 +0100

    send msg ok

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,23 +1,11 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
-// import { dbRefObject } from '../firebase';
 import PeopleList from './chat-components/PeopleList';
 import ChatWindow from './chat-components/ChatWindow';
 import Rooms from './chat-components/Rooms';
 
 
 export default function Chat() {
-    // dbRefObject.on('value', snap => console.log(snap.val()));
-
-    // function writeUserData(userId, name, email, imageUrl) {
-    //     pureDataBase.ref('users/' + userId).set({
-    //         username: name,
-    //         email: email,
-    //         profile_picture: imageUrl
-    //     });
-    // };
-
-    // writeUserData('d121235', 'Olek', 'aleksander@onet.pl', 'https://onet.pl');
 
     return (
         <>
--- src/components/chat-components/ChatWindow.js
+++ src/components/chat-components/ChatWindow.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChatMessage from './ChatMessage';
-import { messagesRef } from '../../firebase';
+import { messagesRef, auth } from '../../firebase';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import firebase from 'firebase';
+import 'firebase/firestore';
+
 export default function ChatWindow() {
     const query = messagesRef.orderBy('createdAt').limit(25);
     const [messages] = useCollectionData(query, { idField: 'id' });
+    const [formValue, setFormValue] = useState('');
+
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        const { uid, photoURL } = auth.currentUser;
+        console.log(uid);
+
+        await messagesRef.add({
+            text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            photoURL
+        })
+        setFormValue('');
+    };
     return (
         <>
             <div className="chat window">
                 {messages && messages.map(msg => <ChatMessage key={msg.id} msg={msg} />)}
             </div>
+            <form onSubmit={sendMessage}>
+                <input type="text" value={formValue} onChange={e => setFormValue(e.target.value)} />
+                <button type='submit'>Send</button>
+            </form>
         </>
     )
 }
