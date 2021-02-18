fce2ecb30ea898469ec25281ac31d0e063188e6e
Alek Kruk <kruk.aleksander@hotmail.com>
2021-02-18 06:15:02 +0100

    fix image problem

--- src/components/chat-components/ChatMessage.js
+++ src/components/chat-components/ChatMessage.js
@@ -3,10 +3,11 @@ import { auth } from '../../firebase';
 
 export default function ChatMessage(props) {
     const { text, uid, photoURL } = props.msg;
+    const photoShowed = photoURL === null ? 'https://cdn.pixabay.com/photo/2017/04/28/07/34/child-2267616_960_720.png' : photoURL;
     const messageClass = uid === auth.currentUser.uid ? 'sent' : 'recived';
     return (
         <div className={`message ${messageClass}`}>
-            <img src={photoURL} alt="" />
+            <img src={photoShowed} alt="" />
             <p>{text}</p>
 
         </div>
